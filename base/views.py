38e2cab3b70751a41b1c7aa1fb067f6d6676adec
Harsh Singh <harsh00var@gmail.com>
2024-05-10 01:01:24 +0530

    completed basic CRUD Operations using Models and Forms

---
+++ base/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Room
+from .forms import RoomForm
+
+# Create your views here.
+# rooms = [
+#     {'id':1, 'name':'Coding in Python'},
+#     {'id':2, 'name':'Design and Analysis of Algorithms'},
+#     {'id':3 ,'name':'Springboot Framework'}
+# ]
+
+def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms':rooms}
+    return  render(request, 'base/home.html',context)
+
+def room(request,pk):
+    room = Room.objects.get(id=pk)
+    context = {'room':room}
+    return render(request,'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()    
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')    
+    context = {'form':form}
+    return  render(request,'base/room_form.html', context)
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    # instance = room will auto fill the form with required room data
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST,instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form':form}
+    return render(request, 'base/room_form.html', context)
